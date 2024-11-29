b1ec9973c4fe0704cfcc8c2eb8f74e6309629028
kvazar <marina810920@gmail.com>
2024-11-29 12:22:18 +0200

    task2 task3

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,6 @@
-// const quantity;
-// const pricePerDroid;
-
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
